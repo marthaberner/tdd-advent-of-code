b88f8086b4f36aaed9d3c7831763478a45e13402
marthaberner <marthaberner.mb@gmail.com>
2017-02-21 22:44:36 -0700

    WIP

--- ruby/lib/day_1.rb
+++ ruby/lib/day_1.rb
@@ -1,3 +1,14 @@
-class SetUp
+class PathFinder
+  def initialize(coords)
+    @coords = coords
+  end
+
+  def get_x2
+
+  end
+
+  def prep
+    @coords.gsub(/\s+/, "").split(',')
+  end
 
 end
--- ruby/spec/day_1_spec.rb
+++ ruby/spec/day_1_spec.rb
@@ -1,8 +1,26 @@
 require 'spec_helper'
 require 'day_1'
 
-describe SetUp do
-  it 'works' do
-    expect(true).to eq(true)
+describe PathFinder do
+  describe "#prep" do
+    it 'turns coords into an array' do
+      input = 'R2, L3'
+      path = PathFinder.new(input)
+      expect(path.prep).to eq(['R2', 'L3'])
+    end
+  end
+
+  describe "#get_x2" do
+    it "returns the value of x2" do
+      input = 'R2, L3'
+      path = PathFinder.new(input)
+      expect(path.get_x2).to eq(2)
+    end
   end
 end
+
+
+# |y1 – y2| + |x1 – x2|
+
+# moves ping pong from x to y
+# first move is always x
